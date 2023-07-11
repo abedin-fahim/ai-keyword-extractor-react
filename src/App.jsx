a698e8363edf490b3b63673d6fe6074a367bff5b
Joynal Abedin <cfcfahim26@gmail.com>
2023-07-11 22:12:38 +0600

    data fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
+import { useState } from 'react';
+
 import { Container, Box } from '@chakra-ui/react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import TextInput from './components/TextInput';
 
 function App() {
-  const extractKeyword = (text) => {
-    console.log(text);
+  const [keywords, setKeywords] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const extractKeyword = async (text) => {
+    setLoading(true);
+    setIsOpen(true);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt:
+          'Extract keywords from this text. Make the first letter of every word uppercase and separate with commas:\n\n' +
+          text +
+          '',
+        temperature: 0.5,
+        max_tokens: 60,
+        top_p: 1.0,
+        frequency_penalty: 0.8,
+        presence_penalty: 0.0,
+      }),
+    };
+
+    const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options);
+    const json = await response.json();
+
+    const data = json.choices[0].text.trim();
+
+    setKeywords(data);
+    setLoading(false);
   };
   return (
     <Box
