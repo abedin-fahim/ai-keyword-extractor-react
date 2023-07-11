b567158f22e5795186240f77fb6ddebb17c0e1fa
Joynal Abedin <cfcfahim26@gmail.com>
2023-07-11 20:29:47 +0600

    chakra ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-function App() {
-  const [count, setCount] = useState(0);
+import { Container, Box } from '@chakra-ui/react';
 
-  return <></>;
+function App() {
+  return (
+    <Box
+      bg='blue.600'
+      color='white'
+      height='100vh'
+      paddingTop={130}
+    >
+      <Container
+        maxW='3xl'
+        centerContent
+      >
+        <h1>My app</h1>
+      </Container>
+    </Box>
+  );
 }
 
 export default App;
